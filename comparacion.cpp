2d620c226359fda0af38d0e9d4b31a630fbf82d6
David-03-As <jose.aguiar.sosa@utelvt.edu.ec>
2022-09-19 10:34:03 -0500

    Numero mayor

---
+++ comparacion.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+ float a,b;
+     cout << "Ingresé a y b: ";
+     cin >>a>>b;	
+     if (a>b)
+    {
+    cout <<a<< " Es mayor que "<<b<<endl;
+    }
+    else
+    {
+    cout <<b<< " Es mayor que "<<a<<endl;
+    }
+    return 0;
+}
